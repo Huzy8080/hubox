3f413d61a8f456baa1fa7d2ec0f8d24e9782f662
Huzy8080 <45865320+Huzy8080@users.noreply.github.com>
2020-05-08 08:37:02 +0800

    feat: websocket协议支持的在线聊天室--channelInitializer

---
+++ src/main/java/com/hubox/demo/netty/chapter12/ChatServerInitializer.java
@@ -0,0 +1,36 @@
+package com.hubox.demo.netty.chapter12;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * TODO
+ *
+ * @author HUZHAOYANG
+ * @version 1.0
+ * @date 2020/5/8 8:31
+ */
+public class ChatServerInitializer extends ChannelInitializer<Channel> {
+    private final ChannelGroup group;
+
+    public ChatServerInitializer(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpServerCodec())
+                .addLast(new ChunkedWriteHandler())
+                .addLast(new HttpObjectAggregator(64 * 1024))
+                .addLast(new HttpRequestHandler("/ws"))
+                .addLast(new WebSocketServerProtocolHandler("/ws"))
+                .addLast(new TextWebSocketFrameHandler(group));
+    }
+}
